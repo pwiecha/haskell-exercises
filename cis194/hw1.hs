348939c240ffb7b333efbe66323d4f57496d3eba
Pawel Wiecha <pawel.wiecha@gmail.com>
2022-08-03 14:19:33 +0200

    cis 194 set1

--- cis194/hw1.hs
+++ cis194/hw1.hs
@@ -20,3 +20,11 @@ sumDigits = sum . map (sum. toDigitsRev)
 -- ex4
 validate :: Integer -> Bool
 validate = (== 0) . (`mod` 10) . sumDigits . doubleEveryOther . toDigitsRev
+
+-- ex5
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n start hlp end = case n of
+    0 -> []
+    n ->  hanoi (n-1) start end hlp ++ [(start, hlp)] ++ hanoi (n-1) end hlp start
