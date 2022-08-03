dfc18ca4c06cef5f69c36fcb65d298fd19c6817a
Pawel Wiecha <pawel.wiecha@gmail.com>
2022-08-03 10:54:25 +0200

    3.5

--- blog_generator/hello.hs
+++ blog_generator/hello.hs
@@ -1,8 +1,25 @@
+-- Top
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
-myhtml = makeHtml "Title" (h1_ "This is a header!" <> p_ "First paragraph")
+-- Html logic
+newtype Html = Html String
+    deriving Show
+
+render :: Html -> String
+render (Html contents) = contents
+
+-- Structure logic
+newtype Structure = Structure String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+type Title = String
+
+-- Program
+myhtml :: Html
+myhtml = Html (makeHtml "Title" (h1_ "This is a header!" <> p_ "First paragraph"))
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
@@ -11,20 +28,20 @@ el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html_ :: String -> String
-html_ = el "html" -- partial application
+html_ :: String -> Structure
+html_ = Structure . el "html" -- partial application
 
-body_ :: String -> String
-body_ = el "body"
+body_ :: String -> Structure
+body_ = Structure . el "body"
 
-head_ :: String -> String
-head_ = el "head"
+head_ :: String -> Structure
+head_ = Structure . el "head"
 
-title_ :: String -> String
-title_ = el "title"
+title_ :: String -> Structure
+title_ = Structure . el "title"
 
-p_ :: String -> String
-p_ = el "p"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-h1_ :: String -> String
-h1_ = el "h1_"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1_"
